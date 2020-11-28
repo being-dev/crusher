50159ad543ae338e286719b75948c3522400059e
Pranit Mhatre <Pranit.Mhatre@global.internal>
2020-11-28 12:13:35 +0530

    Change alert show/hide behaviour

--- js/login.js
+++ js/login.js
@@ -1,6 +1,11 @@
-$('#message').removeClass();
+$(function(){
+    emptyAlert('message');
+});
 
 function fn_login() {
+
+    emptyAlert('message');
+
     var formData = $('#frmLogin').serializeJSON();    
     var hasErrors = checkIfEmptyAndValidate('userId', 'username_field_error', 'Please provide username.');
     if (!hasErrors) hasErrors = checkIfEmptyAndValidate('passwordId', 'password_field_error', 'Please provide password.');
@@ -17,9 +22,7 @@ function fn_login() {
             window.location.href = 'blank.html';
         }).fail(function (error) {
             hideLoader();
-            alert = handleAlert(error);
-            $('#message').addClass('alert').addClass(alert.class);
-            $('#message').html('<span>' + alert.message + '</span>');
+            buildAlert('message',error);
         });
     }
 }
