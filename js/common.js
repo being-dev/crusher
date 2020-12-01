7524df5be7b8a2a41a992c82293f05c12acc61a7
Pranit Mhatre <Pranit.Mhatre@global.internal>
2020-12-01 18:40:06 +0530

    Change to production

--- js/common.js
+++ js/common.js
@@ -99,10 +99,10 @@ function hideLoader() {
 
 function buildUrl(endPoint) {
     var HTTP_PROTOCOL = "https://";
-    var SERVER = "86cfs32wy9";
+    var SERVER = "pkctkbhawb";
     var API = "execute-api";
     var REGION = "ap-southeast-1";
-    var STAGE = "dev";
+    var STAGE = "prod";
     var VENDOR = "amazonaws.com";
     var endPointUrl = HTTP_PROTOCOL;
     endPointUrl = endPointUrl.concat(SERVER).concat('.').concat(API).concat('.').concat(REGION).concat('.').concat(VENDOR);
